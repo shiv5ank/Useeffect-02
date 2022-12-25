97097460431528824bd9a15968b636a32977f4ca
shiv5ank <Shivank0596@gmail.com>
2022-12-25 10:47:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MovieList from './components/MovieList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
